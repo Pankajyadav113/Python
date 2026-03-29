dc022b9a84a118bdf04e08c6996b0db421eefa4b
Pankajyadav113 <pankaj738074@gmail.com>
2026-03-29 09:21:47 +0530

    Day 1: Added Chapter 1 and Chapter 2

---
+++ Chapter 2/Problem.py
@@ -0,0 +1,25 @@
+# a = 1
+# b = 3
+# print("sum ", a+b)
+
+# a = 34 
+# b =  6
+# print("Remainder when a is divided by b is ", a%b)
+
+# a = input("Enput the value of is a:")
+# print(type(a))
+
+# a = int(input("Enter number 1: "))
+# b = int(input("Enter number 2: "))
+# print("a is greater than b is", a>b)
+
+# a =int (input("Enter the value of a is:"))
+# b =int (input("Enter the value of b is:"))
+# print("The average of a and b is:", (a+b)/2)
+
+
+a = int(input("Enter the value of a:"))
+print("The square of a is: ", a**10)
+print("The square of a is: ", a*a)
+# print("The square of a is: ", a^2)  not valid case 
+
---
+++ Chapter 2/variable.py
@@ -0,0 +1,25 @@
+a = 4-2 
+print(a)
+b=6
+b*=3
+print(b)
+
+
+# Comparison operator
+
+d = 5>4
+print(d)
+
+# Logical operator
+
+e = True or False
+print(e)
+
+
+a  = int(input("Enter number 1: "))
+b  = int(input("Enter number 2: "))
+
+print("Number a is : ",a)
+print("Number b is : ",b) 
+print("Sum is ",  a + b) 
+

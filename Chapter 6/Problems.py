d0cd42f955c3be4d86fe6beec3f6ea91a688111f
Pankajyadav113 <pankaj738074@gmail.com>
2026-04-04 22:40:48 +0530

    Added Chapter 6

---
+++ Chapter 6/Problems.py
@@ -0,0 +1,199 @@
+# problem 1
+
+# a1 = int(input("Enter number 1: "))
+# a2= int(input("Enter number 2: "))
+# a3= int(input("Enter number 3: "))
+# a4 = int(input("Enter number 4: "))
+
+# if(a1>a2 and a1>a3 and a1>a4):
+#     print("a1 is the greatest number:", a1)
+# elif(a2>a1 and a2>a3 and a2>a4):
+#     print("a2 is the greatest number:", a2)
+# elif(a3>a1 and a3>a2 and a3>a4):
+#     print("a3 is the greatest number:", a3)
+# elif(a4>a1 and a4>a2 and a4>a3):
+#     print("a4 is the greatest number:", a4)
+
+
+# Problem 2
+
+# sub1 = int(input("Enter the marks: "))
+# sub2 = int(input("Enter the marks: "))
+# sub3 = int(input("Enter the marks: "))
+
+# total_percentage = (100*(sub1 + sub2 + sub3))/300
+# if(total_percentage>=40 and sub1>=33 and sub2>=33 and sub3>=33):
+#     print("You are pass:", total_percentage)
+# else:
+#     print(" Failed! Try again next year:", total_percentage)    
+
+
+
+# Problem 3 
+
+# p1 = "Make a lot of maoney"
+# p2 = "buy now"
+# p3 = "subscrise this"
+# p4 = "click this"
+
+# message = input("Enter your comment: ")
+# if((p1 in message) or (p2 in message) or (p3 in message) or (p4 in message)):
+#     print("This comment is a spam")
+
+# else:
+#     print("This comment is not a spam")    
+
+# problem 4
+
+# username = input("Enter username: ")
+
+# if(len(username)<10):
+#     print("Your username contains less than 10 characters")
+# else:
+#     print("Your userna,me contains more than 10 characters")    
+
+# problem 5
+
+# l = ["Harry", "Shubham", "Pankaj", "Shahid"]
+
+# name = input("Enter your name: ")
+
+# if(name in l):
+#     print("Your name in the list")
+# else:
+#     print("Your name is not in the list")    
+
+
+# problem 6
+
+# marks = int(input("Enter your marks: "))
+
+# if(marks<=100 and marks>=90):
+#     grade = "Ex"
+# elif(marks<=90 and marks>=80):
+#     grade = "A"   
+# elif(marks<=80 and marks>=70):
+#     grade = "B"     
+# elif(marks<=70 and marks>=60):
+#     grade = "C"     
+# elif(marks<=60 and marks>=50):
+#     grade = "D"     
+# elif(marks<50):
+#     grade = "F"
+
+# print("Your grade is:", grade)
+   
+
+# problem 7
+
+# post = input("Enter the post: ")
+
+# if("Harry".lower() in post.lower()):
+#     print("This post is talking about harry")
+
+# else:
+#     print("This post is not talking about harry")    
+
+
+
+
+
+
+
+
+
+# Problem 1
+
+# a = int(input("Enter num1 "))
+# b = int(input("Enter num2 "))
+# c = int(input("Enter num3 "))
+# d = int(input("Enter num4 "))
+
+# if(a>b and a>c and a>d):
+#     print("a is the greatest number", a)
+# elif(b>a and b>c and b>d):
+#     print("b is the greatest number", b)
+# elif(c>a and c>b and c>d):
+#     print("c is the greatest number", c)
+# else:
+#     print("d is the greatest number", d)
+
+
+# Problem 2
+# marks1 = int(input("Enter marks in subject 1 "))
+# marks2 = int(input("Enter marks in subject 2 "))
+# marks3 = int(input("Enter marks in subject 3 "))
+# total_percentage = ((marks1 + marks2 + marks3))/3
+
+# if(total_percentage >= 40 and marks1 > 33 and marks2 > 33 and marks3 > 33):
+#     print("You are pass and your percentage is  ", total_percentage)
+# else:
+#     print("You are fail and your total  percentage is ", total_percentage)   
+
+
+# Problem 3
+
+# p1 = "Make a lot of money"
+# p2 = "buy now"
+# p3 = "subscrise this"
+# p4 = "click this"
+
+# message = input("Enter oyur comment")
+
+# if((p1 in message) or (p2 in message) or (p3 in message) or  (p4 in message)):
+#     print("This is a spam")
+
+# else:
+#     print("This is not a spam")
+
+
+# Problem 4
+
+# Username  = input("Enter your User Name")
+# if(len(Username)<10):
+#     print("Username have less than 10 character")
+
+# else:
+#     print("Username have 10 character")
+
+
+# Problem 5
+
+# a  = ["Pankaj", "Shahid", "Priyanshu", "Himanshu", "Sahil", "Mainuddin"]
+# name   = input("Enter your name ")
+# if(name in a):
+#     print("Your name is in the list")
+# else:
+#     print("Your name is not in the list")    
+
+
+#  # Problem 6  Advanced program
+
+# marks  = int(input("Enter your marks "))
+# if(marks > 100):
+#     print("You enterd invalid marks")
+# else:
+#     if(marks<=100 and marks>=90):
+#         print("Your grade is Ex")
+#     elif(marks<=90 and marks>=80):
+#         print("Your grade is: A")   
+#     elif(marks<=80 and marks>=70):
+#         print("Your grade is B")     
+#     elif(marks<=70 and marks>=60):
+#         print("Your grade is C")     
+#     elif(marks<=60 and marks>=50):
+#         print("Your grade is D")    
+#     else:
+#         print("Your grade is F")   
+
+
+# Problem 7
+
+# # post = "Hey Shahid bhai, kya haal chaal khana khaya ya nahi aur bato kaise ho kya ho raha hai?"
+# post = input("Enter your: ")
+# if("Shahid".lower().upper() in post.lower().upper()):
+#     print("You are talking about Shahid bhai")
+# else:
+#     print("You are not talking about Shahid bahi")    
+
+
---
+++ Chapter 6/conditionals.py
@@ -0,0 +1,62 @@
+# a = int(input("Enter your age: "))
+
+# # if else element
+
+# if(a>=18):
+#     print("Your are above the age os consent")
+#     print("Good for you")   
+
+# else:
+#     print("You are below the age of consent")    
+
+# print("End of  program")
+
+
+# # if elif else ladder
+
+# a = int(input("Enter your age: "))
+
+# if(a>=18):
+#     print("Your are above the age os consent")
+#     print("Good for you")
+# elif(a<0):
+#     print("You are entering an invalid age")
+# elif(a==0):
+#     print("You are entering 0 which is an invalid age")    
+# else:
+#     print("You are below the age of consent")    
+
+# print("End of program")
+  
+
+#  Quiz
+
+# a = int(input("Enter your age: "))
+# if(a>=18):
+#     print("Yes")
+
+
+# # Multiple if statement
+
+
+# a = int(input("Enter your age: "))
+
+# # If statement no. 1
+# if(a%2 == 0):
+#     print("a is even")
+# # End of if statement no. 1
+
+# # If statement no. 2
+# if(a>=18):
+#     print("Your are above the age os consent")
+#     print("Good for you")
+# elif(a<0):
+#     print("You are entering an invalid age")
+# elif(a==0):
+#     print("You are entering 0 which is an invalid age")    
+# else:
+#     print("You are below the age of consent")    
+# # End of if statement no. 2
+
+# print("End of program")
+
